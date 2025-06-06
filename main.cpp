20ecdda78953c0705c5c49000cdd55944ee065b2
Vishudh Shah <vishudh@hotmail.com>
2025-06-05 19:03:53 -0700

    update weight matrices to punish X more

--- main.cpp
+++ main.cpp
@@ -25,26 +25,26 @@ vector<vector<char>> board(BOARD_SIZE, vector<char>(BOARD_SIZE, EMPTY));
 
 // Weighted piece counter for the evaluation function
 const int POSITION_WEIGHTS[BOARD_SIZE][BOARD_SIZE] = {
-    {100, -10, 20, 20, 20, 20, -10, 100},
-    {-10, -20,  1,  1,  1,  1, -20, -10},
-    { 20,   1,  5,  5,  5,  5,   1,  20},
-    { 20,   1,  5,  5,  5,  5,   1,  20},
-    { 20,   1,  5,  5,  5,  5,   1,  20},
-    { 20,   1,  5,  5,  5,  5,   1,  20},
-    {-10, -20,  1,  1,  1,  1, -20, -10},
-    {100, -10, 20, 20, 20, 20, -10, 100}
+    {200,  -10, 20, 20, 20, 20,  -10, 200},
+    {-10, -100,  1,  1,  1,  1, -100, -10},
+    { 20,    1,  5,  5,  5,  5,    1,  20},
+    { 20,    1,  5,  5,  5,  5,    1,  20},
+    { 20,    1,  5,  5,  5,  5,    1,  20},
+    { 20,    1,  5,  5,  5,  5,    1,  20},
+    {-10, -100,  1,  1,  1,  1, -100, -10},
+    {200,  -10, 20, 20, 20, 20,  -10, 200}
 };
 
 // Weighted piece counter for endgame
 const int ENDGAME_WEIGHTS[BOARD_SIZE][BOARD_SIZE] = {
-    {100,   5, 10, 10, 10, 10,   5, 100},
+    {200,   5, 10, 10, 10, 10,   5, 200},
     {  5,  -5,  1,  1,  1,  1,  -5,   5},
     { 10,   1,  1,  1,  1,  1,   1,  10},
     { 10,   1,  1,  1,  1,  1,   1,  10},
     { 10,   1,  1,  1,  1,  1,   1,  10},
     { 10,   1,  1,  1,  1,  1,   1,  10},
     {  5,  -5,  1,  1,  1,  1,  -5,   5},
-    {100,   5, 10, 10, 10, 10,   5, 100}
+    {200,   5, 10, 10, 10, 10,   5, 200}
 };
 
 // Last few moves uses a uniform weight matrix (because at this point all you care about is material, not position)
